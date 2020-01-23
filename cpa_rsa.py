025ef5f606236be554b34da85b1c1a625de6f945
AlexandreLadriere <alexandre.ladriere77@gmail.com>
2020-01-23 11:41:49 +0100

    add: Adding doc for first functions

--- cpa_rsa.py
+++ cpa_rsa.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import math as ma
 
 N_FILE_PATH = "N.txt"
 MSG_TITLE = "msg_"
@@ -7,13 +8,27 @@ TRACE_TITLE = "curve_"
 PATH = "./EMSE/etudiant - 12/"
 NB_MEASURES = 999
 FILE_FORMAT = ".txt"
+KEY_LENGTH = 32
 
 def getModulo(file_path):
+    """
+    Retrieve the n parameter of the RSA algorithm from 
+    the specified text file
+    :return: an integer corresponding to N
+    """
     f = open(file_path, 'r')
     N = f.read()
     return int(N)
 
 def read_entries(type, number):
+    """
+    Reads all the msg and curves file to put their data 
+    into lists of int or float according to their type
+    :param type: Type of data (msg or curve)
+    :param number: Number of file
+    :return: list of int (if type == MSG_TITLE) or 
+    list of list of float (if type == TRACE_TITLE)
+    """
     entries_t = []
     for k in range(number):
         file = open(PATH+type+str(k)+FILE_FORMAT, "r")
@@ -24,9 +39,12 @@ def read_entries(type, number):
                 entries_t.append(int(line.split()[0]))
     return entries_t
 
-# numpy.correlate
-
 def hamming_weight(x):
+    """
+    Calcul the Hamming Weight of the given number
+    :param x: Number you want to have the hamming weight of
+    :return: an integer corresponding to the Hamming weight
+    """
     return bin(x).count("1")
 
 def M_d_mod_N_last_d_bit(M, d_hyp, N):
@@ -34,27 +52,33 @@ def M_d_mod_N_last_d_bit(M, d_hyp, N):
     T = T**2 % N
     if (d_hyp[-1] == 1):
         T = T*M % N
-        return hamming_weight(T)
-    else:
-        return 0
+    return hamming_weight(T), T
 
 if __name__ == "__main__":
     mod = getModulo(PATH + N_FILE_PATH)
-    trace_t = read_entries(TRACE_TITLE, NB_MEASURES)
+    trace_t = np.asarray(read_entries(TRACE_TITLE, NB_MEASURES))
     msg_t = read_entries(MSG_TITLE, NB_MEASURES)
 
-    d_hyp_init = [[0, 0], [0, 1], [1, 0], [1, 1]]
-    C_simul_t = np.zeros((len(msg_t), len(d_hyp_init)))
-    for i in range (len(d_hyp_init)):
-        d = d_hyp_init[i]
-        for k in range (len(msg_t)):
-            msg = msg_t[k]
-            C_simul_t[k, i] = M_d_mod_N_last_d_bit(msg, d, mod)
-    maxi = 0
-    imax = 0
-    for i in range(len(d_hyp_init)):
-        correl = np.correlate(trace_t[:][1], C_simul_t[:, i])
-        if (correl.any() > maxi):
-            maxi = correl.any()
-            imax = i
-    print(d_hyp_init[imax])
+    for bit in range(1):
+        if bit == 0:
+            d_hyp = [[0, 0], [0, 1], [1, 0], [1, 1]]
+        else:
+            d_hyp = [d_hyp + [0], d_hyp + [1]]
+        C_simul_t = np.zeros((len(msg_t), len(d_hyp)))
+        for i in range (len(d_hyp)):
+            d = d_hyp[i]
+            msg_t_i = msg_t
+            for k in range (len(msg_t)):
+                msg = msg_t_i[k]
+                C_simul_t[k, i], msg_t_i[k] = M_d_mod_N_last_d_bit(msg_t_i[k], d, mod)
+        correl = np.zeros((len(d_hyp), len(trace_t[0])))
+        for i in range(len(d_hyp)):
+            for k in range (len(trace_t[0])):
+                correl[i, k] = np.corrcoef(trace_t[:, k], C_simul_t[:, i])
+
+    #     for i in range(len(d_hyp)):
+    #         if (max(correl[i]) > maxi):
+    #             maxi = max(correl[i])
+    #             imax = i
+    #     d_hyp = d_hyp[imax]
+    # print(d_hyp)
