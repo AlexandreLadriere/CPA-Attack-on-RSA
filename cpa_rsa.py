0c395b7afa74ecc8838360125177b38b03eb7c7c
Hugo <hugo.serieys@hotmail.fr>
2020-01-26 10:40:30 +0100

    Still not working

--- cpa_rsa.py
+++ cpa_rsa.py
@@ -31,11 +31,13 @@ def read_entries(type, number):
 def hamming_weight(x):
     return bin(x).count("1")
 
-def M_d_mod_N_last_d_bit(M, T, d_hyp, N):
-    T = T**2 % N
-    if (d_hyp[-1] == 1):
-        T = T*M % N
-    return hamming_weight(T), T
+def M_d_mod_N_last_d_bit(M, d_hyp, N):
+    T = M
+    for i in range(len(d_hyp)-2, -1, -1):
+        T = T**2 % N
+        if (d_hyp[i] == 1):
+            T = T*M % N
+    return hamming_weight(T)
 
 def list_to_binary(x):
     return bin(int(''.join(map(str, x)), 2) << 1)
@@ -45,24 +47,20 @@ if __name__ == "__main__":
     trace_t = np.asarray(read_entries(TRACE_TITLE, NB_MEASURES))
     msg_t = read_entries(MSG_TITLE, NB_MEASURES)
 
-    T_t = [M for M in msg_t]
+    d_hyp = [1]
     for bit in range(32):
-        if bit == 0:
-            d_hyp = [[1, 0], [1, 1]]
-            # d_hyp = [[0], [1]]
-        else:
-            d_hyp = [d_hyp + [0], d_hyp + [1]]
+        d_hyp = [[0] + d_hyp, [1] + d_hyp]
         C_simul_t = np.zeros((len(msg_t), len(d_hyp)))
         for i in range (len(d_hyp)):
             d = d_hyp[i]
             for k in range (len(msg_t)):
                 msg = msg_t[k]
-                T = T_t[k]
-                C_simul_t[k, i], T_t[k] = M_d_mod_N_last_d_bit(msg, T, d, mod)
+                C_simul_t[k, i] = M_d_mod_N_last_d_bit(msg, d, mod)
         correl = np.zeros((len(d_hyp), len(trace_t[0])))
         for i in range(len(d_hyp)):
             for k in range (len([val for val in trace_t[0] if val!=-1000])):
                 correl[i, k] = np.corrcoef(trace_t[:, k], C_simul_t[:, i])[0, 1]
+        print(correl)
 
         maxi = -999
         imax = 0
